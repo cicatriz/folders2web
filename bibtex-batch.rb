5b8cf714c78014e8b0c5af1a5e907d26350b5272
Ryan Muller <cognitionmachine@gmail.com>
2011-09-18 16:47:36 -0400

    reorganize keyword page

--- bibtex-batch.rb
+++ bibtex-batch.rb
@@ -169,10 +169,7 @@ keywords.each do |keyword, pubs|
   kwname = keyword.gsub(/[\,\.\/ ]/,"_").downcase
   keywordslisted << [kwname,keyword,pubs.size]
   File.open("#{Wikipages_path}/kbib/#{kwname}.txt", 'w') {|f| f << out}  
-
-  if not File.exists?("#{Wikipages_path}/#{kwname}.txt")
-    File.open("#{Wikipages_path}/#{kwname}.txt", 'w') { |f| f << "h1. #{keyword}\n\n{{page>kbib:#{kwname}}}" }
-  end
+  File.open("#{Wikipages_path}/#{kwname}.txt", 'w') { |f| f << "h1. #{keyword}\n{{page>notes:#{kwname}}}\n\nh2. Links here\n {{backlinks>.}}\n\n{{page>kbib:#{kwname}}}" }
 
   puts kwname
 end
